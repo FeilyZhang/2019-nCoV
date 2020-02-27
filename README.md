2aadcb7e029ba62a1c06676fc4caae035e11730d
Feily Zhang <fei472625@foxmail.com>
2020-02-27 16:40:52 +0800

    fix some bugs, update the README.md file.

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # 2019-nCoV
 2019-nCoV data platform command line client.
+
+## Getting
+
+If you want source code, you can
+
+```
+git clone https://github.com/FeilyZhang/2019-nCoV.git
+```
+
+If you just want to use it simply, you can download the compressed package [here](https://github.com/FeilyZhang/2019-nCoV/releases/tag/v0.01).
+
+## How to use
+
+You need to install JDK1.8 or jre1.8 (if you just want to run it), configure your environment variables (there are many tutorials on the Internet), and then double-click the bat file to run it.
+
+Of course, you can also add the bat file to the path variable under your environment variable, so that you can run it from the command prompt under CMD, with the command of `2019ncov`.
+
+## Copyright and notices
+
++ This software is licensed through MIT open source agreement.
++ The direct data source of this tool is https://lab.isaaclin.cn/ncov
++ The project address of this tool is https://github.com/FeilyZhang/2019-nCoV
++ If you have any questions, you can submit the issue or email me(fei@feily.tech)
++ Copyright (c) 2020, github.com/FeilyZhang. All rights reserved.
--- src/main/java/tech/feily/nCoV/utils/HttpRequestUtils.java
+++ src/main/java/tech/feily/nCoV/utils/HttpRequestUtils.java
@@ -8,8 +8,6 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 
-import okhttp3.Call;
-import okhttp3.Callback;
 import okhttp3.Headers;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -28,48 +26,41 @@ public class HttpRequestUtils {
     static String saveFile = null;
     
     public static void get(String url, final String type) {
-        System.out.println(url);
         Request req = new Request.Builder()
                 .url(url)
                 .headers(setHeaders())
                 .get()
                 .build();
-        Call call = client.newCall(req);
-        call.enqueue(new Callback() {
-
-            public void onFailure(Call arg0, IOException arg1) {
-                System.out.println("The request failed, details are as followes:\n" + arg1.getMessage() + "\n");
-            }
-
-            public void onResponse(Call arg0, Response arg1) throws IOException {
-                if (arg1.code() == 200) {
-                    System.out.println("Request succeeded, parsing...");
-                    String json = arg1.body().string();
-                    JSONObject obj = JSON.parseObject(json);
-                    String saveFile = desktop + "\\" + System.currentTimeMillis() + ".csv";
-                    FileModel fm = FileUtils.open(saveFile);
-                    if (type.equalsIgnoreCase("prc")) {
-                        OverallModel all = JSONArray.parseObject(JSONObject.toJSONString(obj), OverallModel.class);
-                        FileUtils.write(CSVUtils.getCSVOverallTitle() + "\n", fm);
-                        for (OverallResultsModel rst : all.getResults()) {
-                            FileUtils.write(rst.toString(), fm);
-                        }
-                    } else if (type.equalsIgnoreCase("pro")) {
-                        AreaModel area = JSONArray.parseObject(JSONObject.toJSONString(obj), AreaModel.class);
-                        FileUtils.write(CSVUtils.getCSVAreaTitle() + "\n", fm);
-                        for (AreaResultsModel rst : area.getResults()) {
-                            FileUtils.write(rst.toStringWithoutCities(), fm);
-                        }
+        try {
+            Response res = client.newCall(req).execute();
+            if (res.code() == 200) {
+                System.out.println("Request succeeded, parsing...");
+                String json = res.body().string();
+                JSONObject obj = JSON.parseObject(json);
+                String saveFile = desktop + "\\" + System.currentTimeMillis() + ".csv";
+                FileModel fm = FileUtils.open(saveFile);
+                if (type.equalsIgnoreCase("prc")) {
+                    OverallModel all = JSONArray.parseObject(JSONObject.toJSONString(obj), OverallModel.class);
+                    FileUtils.write(CSVUtils.getCSVOverallTitle() + "\n", fm);
+                    for (OverallResultsModel rst : all.getResults()) {
+                        FileUtils.write(rst.toString(), fm);
                     }
-                    FileUtils.close(fm);
-                    System.out.println("Write file succeeded, path is " + saveFile + "\n");
-                } else {
-                    System.out.println("Unexpected response, response status is " + arg1.isSuccessful() 
-                        + ", response code is " + arg1.code() + ".\n");
+                } else if (type.equalsIgnoreCase("pro")) {
+                    AreaModel area = JSONArray.parseObject(JSONObject.toJSONString(obj), AreaModel.class);
+                    FileUtils.write(CSVUtils.getCSVAreaTitle() + "\n", fm);
+                    for (AreaResultsModel rst : area.getResults()) {
+                        FileUtils.write(rst.toStringWithoutCities(), fm);
+                    }
                 }
+                FileUtils.close(fm);
+                System.out.println("Write file succeeded, path is " + saveFile + "\n");
+            } else {
+                System.out.println("Unexpected response, response status is " + res.isSuccessful() 
+                    + ", response code is " + res.code() + ".\n");
             }
-            
-        });
+        } catch (IOException e) {
+            System.out.println("The request failed, details are as followes:\n" + e.getMessage() + "\n");
+        }
     }
     
 
